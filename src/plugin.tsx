9e1c5dc62dd3ec90187c4e91979b4a2abdb94162
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-12-21 12:15:01 +0530

    refactor for publish

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -3,7 +3,7 @@ import { jsx } from "@emotion/core";
 import ContentstackSDK from "@contentstack/app-sdk";
 
 export default ContentstackSDK.init().then(async (sdk) => {
-    const extensionObj = await sdk["Extension"];
+    const extensionObj = await sdk["location"];
     const RTE = await extensionObj["RTEPlugin"];
 
     return {};
